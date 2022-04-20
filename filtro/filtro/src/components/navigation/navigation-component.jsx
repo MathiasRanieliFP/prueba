766c3f448420e83e9562f3c933d209f00052d252
Mathias Ranieli <mathiasranieli@facephi.com>
2022-04-20 12:11:21 +0200

    lo visual hecho

--- filtro/filtro/src/components/navigation/navigation-component.jsx
+++ filtro/filtro/src/components/navigation/navigation-component.jsx
@@ -1,64 +1,61 @@
-import { Fragment, useState } from "react";
-import { Outlet } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from "reactstrap";
-
+import { Fragment } from "react";
 import "./navigation-style.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
 
 
 export const Navigation = (props) => {
-    const [dropdown, setDropdown] = useState(false);
-    console.log(props)
-    const abrirCerrarDropdown = () =>{
-        setDropdown(!dropdown);
+    function abrirCerrarDropdown() {
+      document.getElementById("filtros").classList.toggle("show");
+      document.getElementById("btn-dropdown").classList.toggle("arreglo")
     }
     const categorias = props.categorias
     const idiomas = props.idiomas 
     
    
-
-    console.log(idiomas + "idi" + categorias +"cats")
     return (
       <Fragment>
         <div className='navigation'>
-            <Dropdown isOpen={dropdown} toggle = {abrirCerrarDropdown} className= "dropdown">
-                <DropdownToggle className="btn-dropdown" caret>Filtrar por... </DropdownToggle>
-                <DropdownMenu>
-                     <div className="categorias"><h6>Categorías</h6> <br/>
+            <div className= "dropdown">
+                <button onClick={abrirCerrarDropdown} id="btn-dropdown" className="btn-dropdown" >Elige una opción <FontAwesomeIcon icon={faArrowDown}/></button>
+                <div className="filtros" id="filtros">
+                     <div className="categorias"><h5>Categorías</h5>
                       {
-                        categorias.map(category =>{console.log(category)
+                        categorias.map(category =>{
                           return(
                             <Fragment key={category.id}>
-                            <p><input type="checkbox" name={category.name}/><a>{category.name}</a></p>
+                            <p><input type="checkbox" name={category.name}/><label>{category.name}</label></p>
                           </Fragment>
                           )
                         })
                       }
                     </div> 
-                    <div className="idioma"> <h6>Idiomas</h6><br/>
+                    <div className="idiomas"> <h5>Idiomas</h5>
                     {
                         idiomas.map(idioma =>{
                           return(
                             <Fragment key={idioma.id}>
-                            <p><input type="checkbox" name={idioma.name}/><a>{idioma.name}</a></p>
+                            <p><input type="checkbox" name={idioma.name}/><label>{idioma.name}</label></p>
                           </Fragment>
                           )
                         })
                       }
                     </div>
-                    <div className="valoracion"><h6>Valoración</h6> <br/>
-                    <input type={"checkbox"}></input><br/>
-                    <input type={"checkbox"}></input><br/>
-                    <input type={"checkbox"}></input><br/>
-                    <input type={"checkbox"}></input><br/>
-                    <input type={"checkbox"}></input>
+                    <div className="valoraciones"><h5>Valoración</h5>
+                    <p><input type={"checkbox"}></input><label><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/></label></p>
+                    <p><input type={"checkbox"}></input><label><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/></label></p>
+                    <p><input type={"checkbox"}></input><label><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/></label></p>                    
+                    <p><input type={"checkbox"}></input><label><FontAwesomeIcon icon={faStar}/><FontAwesomeIcon icon={faStar}/></label></p>                    
+                    <p><input type={"checkbox"}></input><label><FontAwesomeIcon icon={faStar}/></label></p>                    
                     </div>
-                </DropdownMenu>
-            </Dropdown>
+                    <div className="aplicar">
+                        <button className="btn-aplicar">Aplicar</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <Outlet />
       </Fragment>
     );
 };
